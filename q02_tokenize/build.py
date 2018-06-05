cb3bf21b02199020233a932c569981ec7d5e0d85
geraldhood <gerald.hoody@gmail.com>
2018-06-05 05:21:21 +0000

    Done

--- q02_tokenize/build.py
+++ q02_tokenize/build.py
@@ -1,3 +1,4 @@
+
 # Default imports
 
 from nltk.tokenize import TreebankWordTokenizer
@@ -6,4 +7,16 @@ import pandas as pd
 
 from greyatomlib.nlp_day_01_project.q01_load_data.build import q01_load_data
 
+
 # Write your solution here:
+
+def q02_tokenize(path):
+    'write your solution here'
+    twenty_train, X_train, X_test, y_train, y_test = q01_load_data(path)
+    tokenizer = TreebankWordTokenizer()
+    to_tokenize = pd.Series(X_train)
+    almost_tokenized = to_tokenize.apply(lambda row: row.lower())
+    tokenized = almost_tokenized.apply(lambda row: tokenizer.tokenize(str(row)))
+    # X_train.head(20)
+    return tokenized
+
